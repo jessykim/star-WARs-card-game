ed43b8349fdfe250fd96e069c7e6adf351fcea30
jessikim <jessica.kim17@gmail.com>
2022-09-10 22:21:51 -0500

    set constants, variables, and cached element references

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 /*-------------- Constants -------------------*/
-
+const mainDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
 
 /*--------------- Variables ------------------*/
-
+let deck1 = []
+let deck2 = []
+let cardToRemove
+let warDeck1 = []
+let warDeck2 = []
+let deckDisplay1 = []
+let deckDisplay2 = []
+let winner
 
 
 /*---------- Cached Element References -------*/
-
+let deck1El = document.getElementById('deck-1')
+let deck2El = document.getElementById('deck-2')
+let warDeck1El = document.getElementById('war-deck-1')
+let warDeck2El = document.getElementById('war-deck-2')
+let deck1DisplayEl = document.getElementById('deck-1-display')
+let deck2DisplayEl = document.getElementById('deck-2-display')
+let messageEl = document.getElementById('message')
+let dealBtn = document.getElementById('deal-btn')
+let resetBtn = document.getElementById('reset-btn')
 
 
 /*----------- Event Listeners ----------------*/
